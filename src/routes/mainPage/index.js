4bf3dc2a29eb8c0121cd992c15873f1ee0fbf4ce
youngman <sksn12@naver.com>
2022-05-07 18:37:02 +0900

    업스트림 머지 전 작업

--- src/routes/mainPage/index.js
+++ src/routes/mainPage/index.js
@@ -3,18 +3,19 @@ import classNames from 'classnames/bind'
 import { AiOutlinePlus } from 'react-icons/ai'
 import { MdModeEditOutline } from 'react-icons/md'
 import { CgEditBlackPoint } from 'react-icons/cg'
-import TodoCategory from './TodoCategory'
 
 import styles from './MainPage.module.scss'
 import buttonStyles from './components/RoundButton.module.scss'
 import RoundButton from './components/RoundButton'
 import TodoList from './todoList/TodoList'
+import TodoCategory from './TodoCategory'
 
 const cx = classNames.bind(buttonStyles)
 
 function MainPage() {
   return (
     <div className={styles.mainPage}>
+      <TodoCategory />
       <div className={styles.mainPageTodoList}>
         <TodoList />
       </div>
@@ -39,30 +40,27 @@ function FloatButton() {
   }
 
   return (
-    <>
-      <nav>
-        <span className={styles.circularMenu}>
-          <RoundButton
-            onClick={handleAddClick}
-            className={cx(menuOpen ? 'addButtonOpen' : 'addButton')}
-            aria-label='Add button'
-          >
-            <AiOutlinePlus size='2em' />
-          </RoundButton>
-          <RoundButton
-            onClick={handleEditClick}
-            className={cx(menuOpen ? 'editButtonOpen' : 'editButton')}
-            aria-label='Edit button'
-          >
-            <MdModeEditOutline size='1.3em' />
-          </RoundButton>
-        </span>
-        <RoundButton onClick={handleOpenClick} className={buttonStyles.openButton} aria-label='Open button'>
-          <CgEditBlackPoint size='1.5em' />
+    <nav>
+      <span className={styles.circularMenu}>
+        <RoundButton
+          onClick={handleAddClick}
+          className={cx(menuOpen ? 'addButtonOpen' : 'addButton')}
+          aria-label='Add button'
+        >
+          <AiOutlinePlus size='2em' />
         </RoundButton>
-      </nav>
-      <TodoCategory />
-    </>
+        <RoundButton
+          onClick={handleEditClick}
+          className={cx(menuOpen ? 'editButtonOpen' : 'editButton')}
+          aria-label='Edit button'
+        >
+          <MdModeEditOutline size='1.3em' />
+        </RoundButton>
+      </span>
+      <RoundButton onClick={handleOpenClick} className={buttonStyles.openButton} aria-label='Open button'>
+        <CgEditBlackPoint size='1.5em' />
+      </RoundButton>
+    </nav>
   )
 }
 
